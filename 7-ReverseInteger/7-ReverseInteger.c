4c8a6fd737fecfb9ffa3ac0897fdfbe3125686de
hxrjt <160017190+hxrjt@users.noreply.github.com>
2025-07-25 01:50:28 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.c
@@ -0,0 +1,14 @@
+// Last updated: 7/25/2025, 1:50:26 AM
+int reverse(int x){
+    int ans=0;
+    while(x){
+        int temp;
+        temp=x%10;
+        if((ans>INT_MAX/10)||(ans<INT_MIN/10)){
+            return 0;
+        }
+        ans=(ans*10)+temp;
+        x=x/10;
+    }
+    return ans;
+}

e7aa753f5087ba681a63b6d621bb18f88ae8202b
hxrjt <160017190+hxrjt@users.noreply.github.com>
2025-08-01 00:07:21 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/version/cpp/49-GroupAnagrams_20250801_000720.cpp
@@ -0,0 +1,17 @@
+// Last updated: 8/1/2025, 12:07:20 AM
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        map<string,vector<string>> mp;
+        for(string s:strs){
+            string temp=s;
+            sort(temp.begin(),temp.end());
+            mp[temp].push_back(s);
+        }
+        for(auto &i:mp){
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};

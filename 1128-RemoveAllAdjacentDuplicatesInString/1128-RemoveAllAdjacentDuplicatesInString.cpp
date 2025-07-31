55e513283279cbbf2c3da3d83f27a4270b540ed8
hxrjt <160017190+hxrjt@users.noreply.github.com>
2025-07-31 22:15:33 +0530

    Create 1128-RemoveAllAdjacentDuplicatesInString

---
+++ 1128-RemoveAllAdjacentDuplicatesInString/1128-RemoveAllAdjacentDuplicatesInString.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/31/2025, 10:15:33 PM
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int i=0;
+        while(i<s.length()){
+            if(s[i]==s[i+1]){
+                s.erase(i,2);
+                i=0;
+            }
+            else{
+                i++;
+            }
+        }
+        return s;
+    }
+};

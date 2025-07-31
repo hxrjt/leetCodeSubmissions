e098024173b57c738d879f7e33d1c3a0936bfe5a
hxrjt <160017190+hxrjt@users.noreply.github.com>
2025-07-31 22:17:29 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.cpp
@@ -0,0 +1,14 @@
+// Last updated: 7/31/2025, 10:17:28 PM
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> map;
+        for(int i=0;i<nums.size();i++){
+            if(map.find(target-nums[i])!=map.end()){
+                return {i,map[target-nums[i]]};
+            }
+            map[nums[i]]=i;
+        }
+        return {-1,-1};
+    }
+};

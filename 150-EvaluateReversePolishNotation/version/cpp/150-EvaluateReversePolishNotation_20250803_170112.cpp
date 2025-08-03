fd2411315dcac74393e86916b670be5d997a40cb
hxrjt <160017190+hxrjt@users.noreply.github.com>
2025-08-03 17:01:13 +0530

    Create 150-EvaluateReversePolishNotation

---
+++ 150-EvaluateReversePolishNotation/version/cpp/150-EvaluateReversePolishNotation_20250803_170112.cpp
@@ -0,0 +1,36 @@
+// Last updated: 8/3/2025, 5:01:12 PM
+class Solution {
+public:
+    bool isOperator(string s){
+        if(s=="+" || s=="-" || s=="*" || s=="/"){
+            return true;
+        }
+        return false;
+    }
+    int evalRPN(vector<string>& tokens) {
+        stack<int> temp;
+        int ans;
+        for(string s:tokens){
+            if(isOperator(s)){
+                int op2=temp.top();temp.pop();
+                int op1=temp.top();temp.pop();
+                if(s=="+"){
+                    temp.push(op1+op2);
+                }
+                if(s=="-"){
+                    temp.push(op1-op2);
+                }
+                if(s=="/"){
+                    temp.push(op1/op2);
+                }
+                if(s=="*"){
+                    temp.push(op1*op2);
+                }
+            }
+            else{
+                temp.push(stoi(s));
+            }
+        }
+        return temp.top();
+    }
+};

d4057822d61c38171ec58a1e93004b735cd61184
hxrjt <160017190+hxrjt@users.noreply.github.com>
2025-08-03 23:00:13 +0530

    Create 22-GenerateParentheses

---
+++ 22-GenerateParentheses/version/cpp/22-GenerateParentheses_20250803_230011.cpp
@@ -0,0 +1,29 @@
+// Last updated: 8/3/2025, 11:00:11 PM
+class Solution {
+public:
+    void recur(vector<string> &ans,int n,string &s,int nOpen,int nClose){
+        if(nOpen==n && nClose==n){
+            ans.push_back(s);
+            return ;
+        }
+        if(nOpen<n){
+            s+="(";
+            recur(ans,n,s,nOpen+1,nClose);
+            s.pop_back();
+        }
+        if(nOpen>nClose){
+            s+=")";
+            recur(ans,n,s,nOpen,nClose+1);
+            s.pop_back();
+        }
+            
+
+    }
+    vector<string> generateParenthesis(int n) {
+        string s="";
+        vector<string> ans;
+        recur(ans,n,s,0,0);
+        return ans;
+        
+    }
+};

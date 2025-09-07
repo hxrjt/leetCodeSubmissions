05b7b13d066c0c9defaef30a6532e3cf664604e9
hxrjt <160017190+hxrjt@users.noreply.github.com>
2025-09-07 14:49:43 +0530

    Create 1304-FindNUniqueIntegersSumuptoZero

---
+++ 1304-FindNUniqueIntegersSumuptoZero/version/cpp/1304-FindNUniqueIntegersSumuptoZero_20250907_144942.cpp
@@ -0,0 +1,27 @@
+// Last updated: 9/7/2025, 2:49:42 PM
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        if(n==0 || n==1){
+            return {0};
+        }
+        vector<int> ans;
+        int i=n;
+        if(n%2){
+            while(i!=1){
+                ans.push_back(i);
+                ans.push_back(-i);
+                i-=2;
+            }
+            ans.push_back(0);
+        }
+        else{
+            while(i){
+                ans.push_back(i);
+                ans.push_back(-i);
+                i-=2;
+            }
+        }
+        return ans;
+    }
+};

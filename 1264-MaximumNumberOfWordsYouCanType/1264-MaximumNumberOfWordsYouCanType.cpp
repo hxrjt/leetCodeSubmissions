6f89375e6f7b5a9d5b75ef897db6df81b094576f
hxrjt <160017190+hxrjt@users.noreply.github.com>
2025-09-19 00:41:48 +0530

    Create 1264-MaximumNumberOfWordsYouCanType

---
+++ 1264-MaximumNumberOfWordsYouCanType/1264-MaximumNumberOfWordsYouCanType.cpp
@@ -0,0 +1,36 @@
+// Last updated: 9/19/2025, 12:41:46 AM
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        vector<string> words;
+        unordered_set<char> mp;
+        string temp="";
+        for(char c:text){
+            if(c==' '){
+                words.push_back(temp);
+                temp="";
+            }
+            else{
+                temp+=c;
+            }
+        }
+        words.push_back(temp);
+        for(char c:brokenLetters){
+            mp.insert(c);
+        }
+        int count=0;
+        for(int i=0;i<words.size();i++){
+            bool type=true;
+            for(char c:words[i]){
+                if(mp.find(c)!=mp.end()){
+                    type=false;
+                    break;
+                }
+            }
+            if(type){
+                count++;
+            }
+        }
+        return count;
+    }
+};
